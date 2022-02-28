1b29b0b1caea8c03e93db841156c13acc7975d37
DESKTOP-FRJQE58\Mirco <s0547473@htw-berlin.de>
2022-02-28 12:55:32 +0100

    TODOs: Implemented tests for action Move

--- src/test/java/com/slinger/greenfieldworld/model/player/MoveTest.java
+++ src/test/java/com/slinger/greenfieldworld/model/player/MoveTest.java
@@ -1,5 +1,40 @@
 package com.slinger.greenfieldworld.model.player;
 
+import com.slinger.greenfieldworld.model.world.Coordinate;
+import com.slinger.greenfieldworld.model.world.World;
+import com.slinger.greenfieldworld.model.world.WorldGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 class MoveTest {
-    /* TODO: Implement tests. */
+
+    @Test
+    public void triggerWordIsCorrect() {
+
+        Player player = mock(Player.class);
+
+        Move sut = new Move(player);
+
+        assertEquals("move", sut.triggerWord);
+    }
+
+    @Test
+    public void usingMoveChangesPositionOfPlayer() {
+
+        WorldGenerator worldGenerator = new WorldGenerator();
+        World world = worldGenerator.generateWorld("Testopia", 5);
+
+        PlayerGenerator playerGenerator = new PlayerGenerator();
+        Player player = playerGenerator.generatePlayer("Sl1ng3r");
+
+        world.spawnPlayerAtCenter(player);
+
+        Move sut = new Move(player);
+
+        sut.use("north");
+
+        assertEquals(Coordinate.of(2, 1), player.getRegion().getCoordinate());
+    }
 }
