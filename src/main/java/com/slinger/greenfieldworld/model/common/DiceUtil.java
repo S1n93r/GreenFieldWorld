e9007a5f2218a6570e3a1ae8dc2baffe7229e788
DESKTOP-FRJQE58\Mirco <s0547473@htw-berlin.de>
2022-03-31 21:13:05 +0200

    Extended DiceUtil to have a diceRoll with min and max. Also implemented parameterized testing for DiceUtilTest.

--- src/main/java/com/slinger/greenfieldworld/model/common/DiceUtil.java
+++ src/main/java/com/slinger/greenfieldworld/model/common/DiceUtil.java
@@ -9,4 +9,12 @@ public final class DiceUtil {
     public static int rollDice(int sides) {
         return (int) (Math.random() * sides + 1);
     }
+
+    public static int rollDice(int min, int max) {
+
+        if (max <= min)
+            throw new IllegalStateException("Value for 'max' can not be smaller or equal to 'min'.");
+
+        return (int) (Math.random() * ((max - min) + 1) + min);
+    }
 }
--- src/test/java/com/slinger/greenfieldworld/model/common/DiceUtilTest.java
+++ src/test/java/com/slinger/greenfieldworld/model/common/DiceUtilTest.java
@@ -1,6 +1,8 @@
 package com.slinger.greenfieldworld.model.common;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +11,47 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DiceUtilTest {
 
+    /* TODO: Improve test to find missing values as well. Currently it is only tested that min and max are kept.*/
     @Test
-    public void diceResultsAreWithinRange() {
+    public void diceRollWithXSidesIsWithinRange() {
 
+        /* Given */
         List<Integer> diceResults = new ArrayList<>();
 
+        /* When */
         for (int i = 0; i < 100; i++) {
             diceResults.add(DiceUtil.rollDice(6));
         }
 
+        /* Then */
         boolean resultsAreWithinRange = true;
 
         for (int diceResult : diceResults) {
             if (diceResult < 1 || diceResult > 6)
+                resultsAreWithinRange = false;
+        }
+
+        assertTrue(resultsAreWithinRange);
+    }
+
+    /* TODO: Improve test to find missing values as well. Currently it is only tested that min and max are kept.*/
+    @ParameterizedTest
+    @CsvSource({"0,5", "2,4"})
+    public void diceRollWithXMinAndXMaxIsWithinRange(int min, int max) {
+
+        /* Given */
+        List<Integer> diceResults = new ArrayList<>();
+
+        /* When */
+        for (int i = min; i < 100; i++) {
+            diceResults.add(DiceUtil.rollDice(min, max));
+        }
+
+        /* Then */
+        boolean resultsAreWithinRange = true;
+
+        for (int diceResult : diceResults) {
+            if (diceResult < min || diceResult > max)
                 resultsAreWithinRange = false;
         }
 
